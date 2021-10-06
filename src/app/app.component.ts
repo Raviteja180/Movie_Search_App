3023e34fabcc8db25e607aeaaf743a720c31b069
Raviteja <tatipamularaviteja180@gmail.com>
2021-10-06 18:51:06 +0530

    Movie Search App created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MovieSearch';
+  movieName :any = "";
+  url: any;
+  res: any;
+  movies: any;
+  async handleMovies() {
+   this.url = `https://api.themoviedb.org/3/search/movie?api_key=c7e162477b3d1f286cbe911ecc7f73a4&language=en-US&query=${this.movieName}&page=1&include_adult=false`;
+  
+  //  this.url = `https://jsonplaceholder.typicode.com/users`;
+   
+
+     this.res = await fetch(this.url);
+    const data: any = await this.res.json();
+    console.log(data.results);
+    this.movies = data.results;
+  }
 }
